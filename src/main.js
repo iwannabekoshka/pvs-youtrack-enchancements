91554fd04e02f90442b4cc227dbb21224ed677cb
iwannabekoshka <pokusaeff.daniil@gmail.com>
2024-01-10 10:35:23 +0300

    refactor: избавился от вложенности
    Изменил условие, чтоб избавиться от вложенности; изменил цвет текста

--- src/main.js
+++ src/main.js
@@ -1,28 +1,29 @@
 function handleElementAdded(mutationsList, observer) {
   for (const mutation of mutationsList) {
-    if (mutation.type === "childList") {
-      const boardsElem = document.querySelector('[data-test="added-boards"]');
-
-      if (!boardsElem) {
-        continue;
-      }
-
-      const existingTextElement =
-        boardsElem.parentElement.querySelector(".added-boards-text");
-
-      if (existingTextElement) {
-        continue;
-      }
-
-      boardsElem.style.display = "none";
-
-      const textElem = document.createElement("div");
-      textElem.className = "added-boards-text";
-      textElem.textContent = "Здесь были доски 🐱";
-      boardsElem.parentElement.appendChild(textElem);
-
-      observer.disconnect();
+    if (mutation.type !== "childList") {
+      continue;
     }
+
+    const boardsElem = document.querySelector('[data-test="added-boards"]');
+
+    if (!boardsElem) {
+      continue;
+    }
+
+    const existingTextElement =
+      boardsElem.parentElement.querySelector(".added-boards-text");
+
+    if (existingTextElement) {
+      return;
+    }
+
+    boardsElem.style.display = "none";
+
+    const textElem = document.createElement("div");
+    textElem.className = "added-boards-text";
+    textElem.textContent = "Здесь были доски 🐱";
+    textElem.style.color = "hotpink";
+    boardsElem.parentElement.appendChild(textElem);
   }
 }
 
